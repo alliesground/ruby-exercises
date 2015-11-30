6d9180ffb763a3d05f03f6ab946e8f11675dbbd9
Sameer Subba <sameer.libang@gmail.com>
2015-11-30 20:23:01 +1100

    chapter2 ex2 done

---
+++ age.rb
@@ -0,0 +1,7 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "In 10 years you will be:\n #{10 + age}"
+puts "In 20 years you will be:\n #{20 + age}"
+puts "In 30 years you will be:\n #{30 + age}"
+puts "In 40 years you will be:\n #{40 + age}"
