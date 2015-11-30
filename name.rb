5d162b9643e1ec2ab74d18a6dbf2aea08cd156be
Sameer Subba <sameer.libang@gmail.com>
2015-11-30 21:08:43 +1100

    name.rb modified

--- name.rb
+++ name.rb
@@ -1,3 +1,8 @@
 puts "Please type your name"
 name = gets.chomp
-puts "Hello #{name}"
+
+puts "Now enter your last name"
+last_name = gets.chomp
+puts "Hello #{name} #{last_name}"
+
+10.times { puts "#{name} #{last_name}" }
