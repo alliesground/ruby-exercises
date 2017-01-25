35212b59ef19f821a8bde4b4fe8077da119d0d92
Sameer Subba <sameer.libang@gmail.com>
2017-01-25 11:23:45 +1100

    welcome_stranger

--- welcome_stranger.rb
+++ welcome_stranger.rb
@@ -2,15 +2,11 @@ ary = %w(Dan Miller Jones)
 hsh = {title: 'Senior', occupation: 'Developer'}
 
 def greetings(ary, hsh)
-  "Hello, #{name(ary)}! Nice to have a #{title(hsh)} around."
+  "Hello, #{name(ary)}! Nice to have a #{hsh[:title]} #{hsh[:occupation]} around."
 end
 
 def name (ary)
   ary.join(' ')
-end
-
-def title (hsh)
-  hsh.values.join(' ')
 end
 
 puts greetings(ary, hsh)
