ad908bc22c729b7ccd69d38796fb144c2160e615
Sameer Subba <sameer.libang@gmail.com>
2017-01-25 16:39:21 +1100

    grocery list

--- grocery_list.rb
+++ grocery_list.rb
@@ -3,17 +3,7 @@ require 'pry'
 grocery_list = [['grapes', 3], ['apple', 1], ['banana', 4], ['cherry', 10]] 
 
 def convert(grocery_list)
-  all_fruits = []
-
-  grocery_list.each do |fruits|
-    fruit = fruits.first
-    quantity = fruits.last
-
-    quantity.times do
-      all_fruits << fruit
-    end
-  end
-  all_fruits
+  grocery_list.map {|fruit, qty| [fruit] * qty}.flatten
 end
 
 puts convert(grocery_list)
